920059064d10c611257bde4c801e88bc2fae3dd8
abdul al noman <nomanmd1st2001@gmail.com>
2022-10-25 18:58:57 +0600

    create node express and added data

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const port = process.env.port || 5000;
+
+
+const data = require('./data/data.json')
+
+
+app.get('/', (req, res) => {
+    res.send('hello world i am ok')
+})
+
+
+
+app.listen(port, () => {
+    console.log('example check in site', port)
+})
