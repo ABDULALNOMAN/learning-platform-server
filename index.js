0d7aa6262c9bf714c8f4f40a3894a234c4f0cc4e
abdul al noman <nomanmd1st2001@gmail.com>
2022-10-25 21:20:35 +0600

    updata details

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express()
+const cors = require("cors")
 const port = process.env.port || 5000;
 
 
 const data = require('./data/data.json')
 
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('hello world i am ok')
 })
 
-app.get('/data', (req, res) => {
+app.get('/courses', (req, res) => {
     res.send(data)
+})
+
+app.get('/coursesdetails/:id', (req, res) => {
+    const id = req.params.id;
+    const details = data.find(item=>item._id==id)
+    res.send(details)
 })
 
 app.listen(port, () => {
